ef41adee6367ff7a83840805c130533184b36843
YunlongChen <1576302867@qq.com>
2020-01-09 12:01:21 +0800

    更新了tab

---
+++ pages/chat/chat.vue
@@ -0,0 +1,23 @@
+<template name="chat">
+	<view>
+		这是内容哦
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"chat",
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
